21ca2f069f7f7e9dbf0a2dfd7de5ecf1788bc541
Luan B <luan.bernardes@truepay.com.br>
2024-02-18 12:08:36 -0300

    feat: use prettier to format file

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+
+  extends: [
+    "airbnb-base",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
+
+  plugins: ["vue"],
+  rules: {
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": [0, { "packageDir ": "./src/" }],
+    "max-len": [
+      "error",
+      {
+        code: 160,
+        ignorePattern: 'class="([\\s\\S]*?)"|d="([\\s\\S]*?)"', // ignore classes or svg draw attributes
+        ignoreUrls: true,
+      },
+    ],
+    "vue/multi-word-component-names": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".vue", "svg"],
+        moduleDirectory: ["node_modules", "src/"],
+      },
+      alias: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".vue", "svg"],
+        map: [["@", "./src"]],
+      },
+    },
+  },
+};
