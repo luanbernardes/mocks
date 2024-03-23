16c0cf6b2f6a36f3454860e7cffda0c2918b0cea
Luan <luan.bernardes@tino.com.br>
2024-03-23 11:47:54 -0300

    build(deps): remove unnecessary commentary

--- vitest.config.js
+++ vitest.config.js
@@ -8,7 +8,6 @@ export default mergeConfig(
       // enable jest-like global test APIs
       globals: true,
       // simulate DOM with happy-dom
-      // (requires installing happy-dom as a peer dependency)
       environment: 'happy-dom'
     }
   })
